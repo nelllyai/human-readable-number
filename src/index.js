ee45e8f2857529a22d38ea1b37470932a5a65bf8
Nelly Nikitina <worldpiece000@gmail.com>
2023-08-06 17:20:36 +0300

    feat: realise from number to string function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,63 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const stringNumbersBelowTen = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+
+    const stringNumbersAfterNineAndBelowTwenty = [
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+
+    const stringDozens = [
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    const firstNumber = Math.floor(number / 100);
+    const secondNumber = Math.floor(number / 10) % 10;
+    const thirdNumber = number % 10;
+    const lastTwoNumbers = number % 100;
+
+    const words = [];
+
+    if (firstNumber) {
+        words.push(stringNumbersBelowTen[firstNumber] + " hundred");
+    }
+
+    if (lastTwoNumbers > 9 && lastTwoNumbers < 20) {
+        words.push(stringNumbersAfterNineAndBelowTwenty[thirdNumber]);
+    } else {
+        if (secondNumber) {
+            words.push(stringDozens[secondNumber - 2]);
+        }
+
+        if (thirdNumber || (!firstNumber && !secondNumber)) {
+            words.push(stringNumbersBelowTen[thirdNumber]);
+        }
+    }
+
+    return words.join(" ");
+};
